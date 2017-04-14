9185ade4d23875174524479bc71fe6fc7e872efc
Alexandre Resende <alexandre.amres@gmail.com>
2017-04-14 08:57:48 -0300

    Exercises on learn ruby the hard way

---
+++ ex001.rb
@@ -0,0 +1,3 @@
+puts "remaking this exercise"
+puts "I had an issue with a repo on git"
+puts "So I needed to do it all over\nagain"
---
+++ ex002.rb
@@ -0,0 +1,9 @@
+#Learning how to comment in ruby
+#This line won't show up
+#Great for documentation and further explanation on functions
+
+puts 'This will show up'
+
+#some more comments
+
+puts 'Here we go...'
