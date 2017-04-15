2163bc2d924f7f87a2796e974d3e332856d06331
Alexandre Resende <alexandre.amres@gmail.com>
2017-04-15 08:23:21 -0300

    Exercise 5, 6 and 7

---
+++ ex005.rb
@@ -0,0 +1,17 @@
+name = "Alexandre Resende"
+age = 28 #not a lie in 2017
+height = 175 #centimeters
+weight = 60 #kilograms
+eyes = 'brown'
+teeth = 'white'
+hair = 'brown'
+
+puts "Let's talk about #{name}"
+puts "He's #{height} centimeters tall"
+puts "He's #{weight} kilograms heavy"
+puts "Actually that's not heavy"
+puts "He's got #{eyes} and #{hair} hair"
+puts "His teeth are usually #{teeth} dependeding on the coffee"
+
+#I was supposed to make a converter from inches to centimeters
+#and pounds to kilograms...
